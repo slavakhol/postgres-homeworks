0154b633e664040f7c930cc72d4d73cfd5968d43
Slava Kholopov <slava.kholopov@gmail.com>
2023-05-08 18:45:40 +0200

    Прописаны SQL запросы, написан код заполнения БД данными из csv  файлов

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers
+(
+	customer_id varchar(5) PRIMARY KEY,
+	company_name varchar(100) NOT NULL,
+	contact_name varchar(200)
+);
+
+CREATE TABLE employees
+(
+	employee_id serial PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(200),
+	birth_date date,
+	notes text
+	);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(5) REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
+	order_date date,
+	ship_city varchar(100)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,33 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+from datetime import datetime
+import psycopg2, os, csv
+
+with psycopg2.connect(database="north",user="postgres",password=os.getenv('PASSWORD')) as conn:
+    with conn.cursor() as cur:
+        with open('north_data/customers_data.csv', 'r') as f:
+            reader = csv.reader(f)
+            next(reader)
+            for row in reader:
+                customer_id, company_name, contact_name = row
+                cur.execute("INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)", (customer_id, company_name, contact_name))
+
+        with open('north_data/employees_data.csv', 'r') as f:
+            reader = csv.reader(f)
+            next(reader)
+            for row in reader:
+                first_name, last_name, title, birth_date, notes = row
+                date_obj = datetime.strptime(birth_date, '%Y-%m-%d').date()
+                cur.execute("INSERT INTO employees (first_name, last_name, title, birth_date, notes) VALUES (%s, %s, %s, %s, %s)", (first_name, last_name, title, date_obj, notes))
+
+        with open('north_data/orders_data.csv', 'r') as f:
+            reader = csv.reader(f)
+            next(reader)
+            for row in reader:
+                order_id, customer_id, employee_id, order_date, ship_city = row
+                order_id = int(order_id)
+                employee_id = int(employee_id)
+                date_obj = datetime.strptime(order_date, '%Y-%m-%d').date()
+                cur.execute("INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) VALUES (%s, %s, %s, %s, %s)", (order_id, customer_id, employee_id, date_obj, ship_city))
+
+
+conn.close()
